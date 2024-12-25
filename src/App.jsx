d83964d206c0fa625087596a8c86e6a4585a7505
pujanjoci <pujanjoci2@gmail.com>
2024-12-25 14:34:46 +0545

    Added Basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import Home from './pages/Home';
 import About from './pages/About'; // Import the About component
 import Contact from './pages/Contact'; // Import the Contact component
@@ -9,15 +9,17 @@ import './index.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Home route */}
-        <Route path="/about" element={<About />} /> {/* About route */}
-        <Route path="/contact" element={<Contact />} /> {/* Contact route */}
-        <Route path="/faq" element={<FAQ />} /> {/* FAQ route */}
-      </Routes>
-    </div>
+    <BrowserRouter basename="/clother-site">
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home route */}
+          <Route path="/about" element={<About />} /> {/* About route */}
+          <Route path="/contact" element={<Contact />} /> {/* Contact route */}
+          <Route path="/faq" element={<FAQ />} /> {/* FAQ route */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
